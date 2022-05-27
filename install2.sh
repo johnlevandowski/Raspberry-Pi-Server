cca4b5dc11e76bb19d294dee711ce586b0caa07e
John Levandowski <john@johnlevandowski.com>
2022-05-27 13:42:19 -0600

    add fixed IP address to /etc/hosts

--- install2.sh
+++ install2.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Variables
+FIXEDIP="192.168.0.200"
+
 echo "# Enable SSH"
 sudo systemctl enable ssh
 echo ""
@@ -13,10 +16,13 @@ echo "# Configure Static IP Address"
 NETWORKCONF="/etc/dhcpcd.conf"
 echo '' | sudo tee -a $NETWORKCONF > /dev/null
 echo 'interface eth0' | sudo tee -a $NETWORKCONF > /dev/null
-echo 'static ip_address=192.168.0.200/24' | sudo tee -a $NETWORKCONF > /dev/null
+echo 'static ip_address='$FIXEDIP'/24' | sudo tee -a $NETWORKCONF > /dev/null
 echo 'static routers=192.168.0.1' | sudo tee -a $NETWORKCONF > /dev/null
 echo 'static domain_name_servers=192.168.0.1' | sudo tee -a $NETWORKCONF > /dev/null
 tail -n 6 $NETWORKCONF
+echo ""
+sudo sed -i 's/127.0.1.1/'$FIXEDIP'/' /etc/hosts
+cat /etc/hosts
 echo ""
 
 echo "# Add User"
