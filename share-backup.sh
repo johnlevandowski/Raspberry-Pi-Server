cd53782e109a9e472fe7f9c48952da7f9f78f4a1
John Levandowski <john@johnlevandowski.com>
2022-08-25 18:57:07 -0600

    exclude libreoffice lock files

--- share-backup.sh
+++ share-backup.sh
@@ -30,7 +30,7 @@ YEAR=$(echo $DATE | cut -c1-4)
 MONTH=$(echo $DATE | cut -c5-6)
 DAY=$(echo $DATE | cut -c7-8)
 HOUR=$(echo $DATE | cut -c9-10)
-EXCLUDE="--exclude .DS_Store --exclude desktop.ini --exclude thumbs.db --exclude *.itc2"
+EXCLUDE="--exclude .DS_Store --exclude desktop.ini --exclude thumbs.db --exclude *.itc2 --exclude .~lock.*"
 
 # Once an Hour Backup
 
